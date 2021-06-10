6d5d1577789e72862b7c8d7ab2e11d6a49223718
Himanshu Singhal <singhal@global.cadence.com>
2021-06-10 17:45:54 +0530

    Added code for handling diagonally non-dominant matrix

--- MFDS_Assignments/g_jac_sei.py
+++ MFDS_Assignments/g_jac_sei.py
@@ -16,20 +16,48 @@ while diag_dom_condition != True:
 	(np.abs(a[1][1])>np.abs(a[1][0])+np.abs(a[1][2])) and \
 	(np.abs(a[2][2])>np.abs(a[2][0])+np.abs(a[2][1]))
 
+# Generate matrix which can't be made diagonally dominant
+# Note: This is for augmented matrix 
+# i.e. for mxn matrix, mx(n-1) matrix would be diagonally non-dominant
+size = int(input("Enter size of matrix: "))
+
+flag = 0
+while (flag != size*size):
+	flag = 0
+	b = 20 * random_state.rand(size,size+1)
+	b = np.around(b, decimals=4)
+	for i in range(len(b[:,0])):
+		for j in range(len(b[0,:])-1):
+			if abs(b[i][j]) < np.sum(abs(b[i][:size])) - abs(b[i][j]):
+				flag += 1
+			else:
+				flag -= 1
+
+print("This is your diagonally dominant matrix:")
 print(a)
+
+print("This is your diagonally non-dominant matrix:")
+print(b)
+
+diag_dom = int(input("Enter the condition: 0 for digonally dominant, 1 for digonally non-dominant: "))
+
+if diag_dom == 0:
+	c = copy.deepcopy(a)
+elif diag_dom == 1:
+	c = copy.deepcopy(b)
 
 # Defining equations to be solved
 # in diagonally dominant form
-f1 = lambda x,y,z: (a[0][3]-a[0][1]*y-a[0][2]*z)/a[0][0]
-f2 = lambda x,y,z: (a[1][3]-a[1][0]*x-a[1][2]*z)/a[1][1]
-f3 = lambda x,y,z: (a[2][3]-a[2][0]*x-a[2][1]*y)/a[2][2]
+f1 = lambda x,y,z: (c[0][3]-c[0][1]*y-c[0][2]*z)/c[0][0]
+f2 = lambda x,y,z: (c[1][3]-c[1][0]*x-c[1][2]*z)/c[1][1]
+f3 = lambda x,y,z: (c[2][3]-c[2][0]*x-c[2][1]*y)/c[2][2]
 
 # Implement Gauss Seidal Method
 def gauss_seidal(x,y,z,e):
 	count = 1
 	condition = True
 	print('\nCount\tx\ty\tz\n')
-	while condition:
+	while (condition and count<50):
 		# In Gauss Seidal, newest available
 		# values of variables are used
 		x1 = round(f1(x,y,z),4)
@@ -45,7 +73,7 @@ def gauss_seidal(x,y,z,e):
 		y = y1
 		z = z1
 
-		condition = e1>e and e2>e and e3>e
+		condition = e1>e or e2>e or e3>e
 	print('\nSolution from Gauss Seidal Method: x=%0.4f, y=%0.4f and z = %0.4f\n'% (x1,y1,z1))
 
 # Implement Gauss Jacobi Method
@@ -53,7 +81,7 @@ def gauss_jacobi(x,y,z,e):
 	count = 1
 	condition = True
 	print('\nCount\tx\ty\tz\n')
-	while condition:
+	while (condition and count<50):
 		# In Gauss Jacobi, values of variables
 		# available from last iteration are used
 		x1 = round(f1(x,y,z),4)
@@ -69,7 +97,7 @@ def gauss_jacobi(x,y,z,e):
 		y = y1
 		z = z1
 
-		condition = e1>e and e2>e and e3>e
+		condition = e1>e or e2>e or e3>e
 
 	print('\nSolution from Gauss Jacobi Method: x=%0.4f, y=%0.4f and z = %0.4f\n'% (x1,y1,z1))
 
